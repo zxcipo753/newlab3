f3ccbc61376b0dbb5213d65818955df9fe404265
zxcipo753 <60366443+zxcipo753@users.noreply.github.com>
2021-09-29 14:10:20 -0400

    improve listener and layout

--- app/src/main/java/edu/temple/cis3515_assigment_3/ImageActivity.kt
+++ app/src/main/java/edu/temple/cis3515_assigment_3/ImageActivity.kt
@@ -7,22 +7,21 @@ import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val dogdata: Array<ImageObject> = getData()
         val recycle = findViewById<RecyclerView>(R.id.rcvView) as RecyclerView
-        var image = findViewById<ImageView>(R.id.mainImageView)
         var text = findViewById<TextView>(R.id.dog)
+        var image = findViewById<ImageView>(R.id.mainImageView)
         var adapter =ImageAdapter(dogdata)
         recycle.adapter = adapter
         adapter.setOnItemClickListener(object : ImageAdapter.onItemClickListener{
             override fun onItemClick(position: Int) {
-
                 image.setImageResource(dogdata[position].resourceId)
                 text.text = dogdata[position].description
-                text.textSize = 20F
+                text.textSize = 22F
             }
         })
         recycle.layoutManager = GridLayoutManager(this,3)
@@ -35,7 +34,6 @@ class MainActivity : AppCompatActivity() {
             ImageObject("doberman", R.drawable.doberman),
             ImageObject("goldenretriever",R.drawable.goldenretriever),
             ImageObject("bulldog",R.drawable.bulldog),
-            ImageObject("samoyed",R.drawable.samoyed),
             ImageObject("pomeranian",R.drawable.pomeranian),
             ImageObject("havanese",R.drawable.havanese),
             ImageObject("corgi",R.drawable.corgi),
